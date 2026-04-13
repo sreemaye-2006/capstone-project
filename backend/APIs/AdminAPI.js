307ba01e662ab42461a3bfa413ba7ca5e2317f4f
katukojwala Sreemaye <Sirisreemaye@gmail.com>
2026-04-13 20:05:40 +0530

    Update AdminAPI.js
    Added routes to get, block, and unblock users.

--- backend/APIs/AdminAPI.js
+++ backend/APIs/AdminAPI.js
@@ -1,2 +1,42 @@
-import exp from 'express'
-export const adminApp=exp.Router()
+import exp from "express";
+export const adminApp = exp.Router();
+
+import { UserModel } from "../models/UserModel.js";
+
+// GET ALL USERS & AUTHORS
+adminApp.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.send({ message: "Users list", payload: users });
+  } catch (err) {
+    res.send({ message: "Error", payload: err.message });
+  }
+});
+
+// BLOCK USER / AUTHOR
+adminApp.put("/block/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      { isUserActive: false },
+      { new: true }
+    );
+    res.send({ message: "User blocked", payload: user });
+  } catch (err) {
+    res.send({ message: "Error blocking user", payload: err.message });
+  }
+});
+
+// UNBLOCK USER / AUTHOR
+adminApp.put("/unblock/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      { isUserActive: true },
+      { new: true }
+    );
+    res.send({ message: "User unblocked", payload: user });
+  } catch (err) {
+    res.send({ message: "Error unblocking user", payload: err.message });
+  }
+});
