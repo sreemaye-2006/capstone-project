ae6e97bd0a0145b750dd7e065863e1935659f24d
katukojwala Sreemaye <Sirisreemaye@gmail.com>
2026-04-13 21:01:51 +0530

    Update AdminAPI.js

--- backend/APIs/AdminAPI.js
+++ backend/APIs/AdminAPI.js
@@ -1,42 +1,30 @@
-import exp from "express";
-export const adminApp = exp.Router();
-
-import { UserModel } from "../models/UserModel.js";
-
-// GET ALL USERS & AUTHORS
-adminApp.get("/users", async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.send({ message: "Users list", payload: users });
-  } catch (err) {
-    res.send({ message: "Error", payload: err.message });
-  }
-});
-
-// BLOCK USER / AUTHOR
-adminApp.put("/block/:id", async (req, res) => {
-  try {
-    const user = await UserModel.findByIdAndUpdate(
-      req.params.id,
-      { isUserActive: false },
-      { new: true }
-    );
-    res.send({ message: "User blocked", payload: user });
-  } catch (err) {
-    res.send({ message: "Error blocking user", payload: err.message });
-  }
-});
-
-// UNBLOCK USER / AUTHOR
-adminApp.put("/unblock/:id", async (req, res) => {
-  try {
-    const user = await UserModel.findByIdAndUpdate(
-      req.params.id,
-      { isUserActive: true },
-      { new: true }
-    );
-    res.send({ message: "User unblocked", payload: user });
-  } catch (err) {
-    res.send({ message: "Error unblocking user", payload: err.message });
-  }
-});
+import exp from 'express'
+import { verifyToken } from '../middlewares/VerifyToken.js'
+import { ArticleModel } from '../models/ArticleModel.js'
+import { UserModel } from '../models/UserModel.js'
+export const AdminApp=exp.Router()
+//login completed in common api 
+//view all articles
+AdminApp.get("/articles",verifyToken("ADMIN"),async(req,res)=>{
+    const articles=await ArticleModel.find()
+    res.status(200).json({message:"all articles are as follows",payload:articles})
+})
+//view all users
+AdminApp.get("/users",verifyToken("ADMIN"),async(req,res)=>{
+    const users=await UserModel.find()
+    res.status(200).json({message:"all users are as follows",payload:users})
+})
+//block and activate user
+AdminApp.patch("/user/:id",verifyToken("ADMIN"),async(req,res)=>{
+    const {_id,isActive}=req.body
+    const user=await UserModel.findById(_id)
+    if(!user){
+        return res.status(404).json({message:"user not found"})
+    }
+    if(user.isActive===isActive){
+        return res.status(200).json({message:"user already in same state"})
+    }
+    user.isActive=isActive
+    await user.save()
+    res.status(200).json({message:"user status updated",payload:user})
+})
