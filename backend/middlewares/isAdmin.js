1c4537eb50abe618d24d6067c56fed9f971f18b2
katukojwala Sreemaye <Sirisreemaye@gmail.com>
2026-04-13 20:06:04 +0530

    Delete backend/middlewares/isAdmin.js

--- backend/middlewares/isAdmin.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
-    return res.status(403).json({ message: "Admin access required" });
-  }
-  next();
-};
-
-module.exports = isAdmin;
